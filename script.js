f653769401da8dfb7253e4ef0b64c7166ec3024e
Maike2961 <mayki.silva@hotmail.com>
2024-07-20 20:19:08 -0300

    Adicionado um carrinho de compras

--- script.js
+++ script.js
@@ -8,44 +8,85 @@ function adicionarPizza(event) {
     const pizainfos = botao.parentElement.parentElement
     const pizzaImage = pizainfos.getElementsByClassName("pizza-image")[0].src
     const pizzaName = pizainfos.getElementsByTagName("h3")[0].innerText
-    const pizzaFatia0 = pizainfos.getElementsByTagName("h4")[0].innerText
-    const pizzaFatia1 = pizainfos.getElementsByTagName("h4")[1].innerText
-    const pizzaFatia2 = pizainfos.getElementsByTagName("h4")[2].innerText
-
-    const pizzah4 = pizainfos.getElementsByTagName("span")[0].innerText
-    console.log(pizzaFatia0.split(""))
-
     const tableBody = document.getElementById("lista")
 
     tableBody.innerHTML = ''
     tableBody.innerHTML += `
-    <td class="carrinho-image">
-        <img src="${pizzaImage}" alt="${pizzaName}">
-        <strong class="pizza-name">${pizzaName}</strong>
-    </td>
-    <td>
-        <span class="Pizza-fatia">${pizzaFatia0}</span>
-        <span class="Pizza-fatia">${pizzaFatia1}</span>
-        <span class="Pizza-fatia">${pizzaFatia2}</span>
-    </td>
-    <td>
-        <input type="number" value="1" min="0" class="product-qtd-input">
-        <button type="button" class="remove-product-button">Remover</button>
-    </td>
+    <tr>
+        <td class="carrinho-image">
+            <img src="${pizzaImage}" alt="${pizzaName}">
+            <strong class="pizza-name">${pizzaName}</strong>
+        </td>
+        <td>
+            <input type="number" value="1" min="0" class="pizza-qtd-input">
+        </td>
+        <td>
+            <select id="SelectFatia">
+                <option value="35,00">6 fatias</option>
+                <option value="45,00">8 fatias</option>
+                <option value="55,00">12 fatias</option>
+            </select>
+        </td>
+        <td>
+            <div class="menu-carrinho">
+                <div class="one"></div>
+                <div class="three"></div>
+            </div>
+        </td>
+    </tr>
     `
+
     abrircarrinho()
+    calcular()
+    const QtdTable = document.querySelector(".pizza-qtd-input")
+    const PrecoTable = document.querySelector("#SelectFatia")
+
+    QtdTable.addEventListener("change", calcular)
+    PrecoTable.addEventListener("change", calcular)
 }
+
+const calcular = () =>{
+    const rows = document.querySelectorAll("#lista tr")
+    const total = document.getElementById("total")
+    let soma = 0;
+    console.log(rows)
+    rows.forEach(element => {
+        let preco = element.querySelector("#SelectFatia")
+        console.log(preco)
+        let quantidade = element.querySelector(".pizza-qtd-input")
+        console.log(quantidade)
+        if(preco && quantidade != null){
+            let precovalor = parseFloat(preco.value)
+            let Quantidadevalor = parseInt(quantidade.value)
+            let subTotal = Quantidadevalor * precovalor
+            soma += subTotal
+        }
+    });
+    console.log(soma)
+    total.innerHTML = soma.toFixed(2)
+    
+}
+
 
 function abrircarrinho(event) {
     let show = true
     const carro = document.querySelector(".carrinho")
-    console.log(carro)
     carro.classList.toggle('on', show)
     carro.style.width = "100vw"
-    carro.style.height = "30vh"
-    carro.style.transition = "ease-out 0.8s 5s"
+    carro.style.height = "60vh"
     show = !show;
+    fecharcarrinho()
 }
+
+function fecharcarrinho(){
+    const menuCarrinho = document.querySelector(".menu-carrinho")
+    const carro = document.querySelector(".carrinho.on")
+    
+    menuCarrinho.addEventListener('click', function(){
+        carro.classList.remove('on')
+    })
+}
+
 
 function menu() {
     let show = true
